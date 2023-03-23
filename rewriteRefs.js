3394649629411b176e9f53142fa0483bce558604
Ford Hideo Bennett <ford660bennett@tamu.edu>
2023-03-22 21:35:20 -0500

    Added documentation to rewriteRefs

--- rewriteRefs.js
+++ rewriteRefs.js
@@ -3,6 +3,10 @@ const fs = require('fs-extra')
 
 const config = require('./config')
 
+/**
+ * It reads the `packed-refs` file, replaces the pull request references with branch references, and
+ * writes the file back
+ */
 const main = async () => {
   const headRegEx = /refs\/heads\/pr([0-9]+)head/
   const refsPath = `${config.source.repo}/.git/packed-refs`
