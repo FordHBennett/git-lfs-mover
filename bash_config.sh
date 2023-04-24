583e2d620559cce49d4a068897b1b45ad381d978
Ford Hideo Bennett <ford660bennett@tamu.edu>
2023-04-24 13:32:27 -0500

    Bash config file

--- bash_config.sh
+++ bash_config.sh
@@ -1,11 +1,37 @@
 # Source repo ssh
-SRC_SSH="git@github.com:username/source_repo.git"
+SRC_SSH="git@github.tamu.edu:SpaceCRAFT/Platform.git"
 
 # Target repo ssh
-TARGET_SSH="git@github.com:username/target_repo.git"
+TARGET_SSH="git@github.com:SimDynamX/SC_Platform.git"
 
 # Source repo directory path
-REPO_DIR="/path/to/local/source/directory"
+REPO_DIR="Platform"
 
 # Source repo mirror path
-REPO_MIRROR="/path/to/local/source/directory.git"
+REPO_MIRROR="Platform.git"
+
+#Testing the source config
+src_test(){
+    # Run the node test.js script
+    node test.js source
+
+    # Check the exit status of the last executed command
+    if [ $? -eq 1 ]; then
+    echo "Error: test.js script failed"
+    echo "Please check your source config variables in config.js"
+    exit 1
+    fi
+}
+
+#Testing the target config
+target_test(){
+    # Run the node test.js script
+    node test.js target
+
+    # Check the exit status of the last executed command
+    if [ $? -eq 1 ]; then
+    echo "Error: test.js script failed"
+    echo "Please check your target config variables in config.js"
+    exit 1
+    fi
+}
