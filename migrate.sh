93b59c366b2e255ad5dcb5bdb5a7fd58c9df31c8
Ford Hideo Bennett <ford660bennett@tamu.edu>
2023-04-26 08:32:28 -0500

    push_small_commits_to_branch pushes every refs/heads/*

--- migrate.sh
+++ migrate.sh
@@ -13,38 +13,56 @@ readonly REPO_DIR
 # Source repo mirror path
 readonly REPO_MIRROR
 
-
-
-# Pushing small commits to the given branch.
-push_small_commits_to_branch()
+push_lfs_objects_to_branch()
 {
     local branch="$1"
-    printf "Pushing small commits to: %s\n" "$branch"
+    printf "Pushing lfs objects to: %s\n" "$branch"
 
     # Checkout the given branch
-    echo $(git switch -C "$branch")
-    echo "$(git lfs fetch)"
+    # echo $(git switch -C "$branch")
+    echo "$(git checkout "$branch")"
+    echo "$(git lfs fetch "$branch")"
     echo "$(git lfs checkout)"
-
-    # Reverse the order of commits and push them one by one
-    count=0
-    for commit in $(git rev-list --reverse $branch | ruby -ne 'i ||= 0; i += 1; puts $_ if i % 100 == 0'); do
-        echo $(git push --force "$TARGET_SSH" "$commit:refs/heads/$branch")
-        count=$((${count} + 1))
-    done
-
-    # Push tags related to the given branch
-    echo $(git push --force "$TARGET_SSH" --tags "$branch" )
 
     # Push all git-lfs files to the given branch
     echo $(git lfs push "$TARGET_SSH" "$branch" )
 }
+# Pushing small commits to the given branch.
+push_small_commits_to_branch()
+{
+    local branch="$1"
+    local hash="$2"
+    printf "Pushing small commits to: %s\n" "$branch"
+
+    echo $(git push "$TARGET_SSH" "$(git rev-list --reverse --max-count=1 $hash):$branch" )
+
+    # Reverse the order of commits and push them one by one
+    for commit in $(git rev-list --reverse $hash | ruby -ne 'i ||= 0; i += 1; puts $_ if i % 10 == 0'); do
+        echo $(git push --force "$TARGET_SSH" "$commit:$branch")
+    done
+
+    # Push tags related to the given branch
+    echo $(git push "$TARGET_SSH" "$branch" )
+    echo $(git push "$TARGET_SSH" "$branch" --tags)
+}
 push_small_commits_in_parallel() {
     local arr=("$@")
+    local refs=()
+    local hashes=()
+
+    for((i=0; i<${#arr[@]}/2; i+=1)); do
+        refs+=(${arr[i]})
+    done
+    for((i=${#arr[@]}/2; i<${#arr[@]}; i+=1)); do
+        hashes+=(${arr[i]})
+    done
+
+
+
     export -f push_small_commits_to_branch # Export the function to make it available for parallelization
 
-    for branch in "${arr[@]}"; do
-        push_small_commits_to_branch "$branch" &
+    for ((i=0; i<"${#refs[@]}"; i+=1)); do
+        push_small_commits_to_branch "${refs[i]}" "${hashes[i]}" &
     done
     wait
 }
@@ -57,217 +75,201 @@ display_txt_file() {
 }
 main()
 {
-    # Run the node test.js script
-    echo "$(src_test)"
-    echo "$(target_test)"
+    # # Run the node test.js script
+    # echo "$(src_test)"
+    # echo "$(target_test)"
 
 
-    if clone_mirror=$(git clone --mirror "$SRC_SSH" "$REPO_MIRROR" >/dev/null 2>&1); then
-      printf '%s
-    ' "$clone_mirror"
-      echo "Successfully cloned $SRC_SSH to $REPO_MIRROR"
-    else
-      echo "Failed to clone $SRC_SSH; exited with $?" >&2
-      exit 1
-    fi
+    # if clone_mirror=$(git clone --mirror "$SRC_SSH" "$REPO_MIRROR" >/dev/null 2>&1); then
+    #   printf '%s
+    # ' "$clone_mirror"
+    #   echo "Successfully cloned $SRC_SSH to $REPO_MIRROR"
+    # else
+    #   echo "Failed to clone $SRC_SSH; exited with $?" >&2
+    #   exit 1
+    # fi
 
+    # if fetch=$(node fetch.js 2>&1); then
+    #   printf '%s
+    # ' "$fetch"
+    # else
+    #   echo "node fetch.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
+    # if rewriteRefs=$(node rewriteRefs.js 2>&1); then
+    #   printf '%s
+    # ' "$rewriteRefs"
+    # else
+    #   echo "node rewriteRefs.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    # Create a new directory to store the cloned repository and move the mirrored repository inside
-    mkdir "$REPO_DIR"
-    mv "$REPO_MIRROR" "$REPO_DIR"
-    cd "$REPO_DIR"
+    # cd "$REPO_MIRROR"
 
-    mv "$REPO_MIRROR" ".git"
-    git init
-    git config --bool core.bare false
+    # refs=$(cat packed-refs | awk '$2 ~ /^refs\/heads/{print $2}')
+    # refs_array=($refs)
+    # hashes=$(cat packed-refs | awk '$2 ~ /^refs\/heads/{print $1}')
+    # hashes_array=($hashes)
 
-    if fetch_all=$(git fetch --all --tags --update-head-ok 2>&1); then
-      printf '%s
-      ' "$fetch_all"
-    else
-      echo "git fetch failed; exited with $?" >&2
-      exit 1
-    fi
+    # for ((i=0; i<${#hashes_array[@]}; i++)); do
+    #   local commit_count="$(git rev-list --count ${hashes_array[i]})"
+    #   local dummy_hash="${hashes_array[i]}"
+    #   local dummy_ref="${refs_array[i]}"
+    #   echo "i: $i"
+    #   j=$((i-1))
+    #   while ((j>=0)) && [[ "$(git rev-list --count ${hashes_array[j]})" > "$commit_count" ]]; do
+    #     refs_array[j+1]="${refs_array[j]}"
+    #     hashes_array[j+1]="${hashes_array[j]}"
+    #     j=$((j-1))
+    #     echo "j: $j"
+    #   done
+    #   refs_array[j+1]="$dummy_ref"
+    #   hashes_array[j+1]="$dummy_hash"
+    # done
 
+    # mkdir "branches"
+    # cd "branches"
 
-    if fetch_lfs_all=$(git lfs fetch --all 2>&1); then
-      printf '%s
-    ' "$fetch_lfs_all"
-    else
-      echo "git lfs fetch failed; exited with $?" >&2
-      exit 1
-    fi
+    # echo "${refs_array[@]}" > refs.txt
+    # echo "${hashes_array[@]}" > hashes.txt
 
-    pr_commits=($(git for-each-ref --format='%(refname:lstrip=2):%(objectname)'))
-    for pr_commit in "${pr_commits[@]}"; do
-      pr_commit_split=(${pr_commit//:/ })
-      pr_commit_name=${pr_commit_split[0]}
-      pr_commit_hash=${pr_commit_split[1]}
-      if [[ "$pr_commit_name" =~ pr.*head ]]; then
-        echo "$(git checkout -f -b "$pr_commit_name" "$pr_commit_hash")"
-      fi
-    done
+    # for ((i=0; i<${#hashes_array[@]}; i++)); do
+    #   echo "${hashes_array[i]} ${refs_array[i]}"
+    # done
 
-    # loop through all branches, both local and remote
-    for branch in $(git branch -a); do
-      if [[ ! "$branch" =~ "HEAD" ]]; then
-        local_branch=${branch#remotes/origin/} # remove the remote prefix
-        if [[ ! "$local_branch" =~ ^master$|^dev$ ]]; then # skip master and dev branch
-          if ! git show-ref --verify --quiet refs/heads/$local_branch; then # check if the branch exists locally
-            git branch --track "$local_branch" "$branch" && echo "$local_branch" created and set up to track "$branch"
-          fi
-        fi
-      fi
-    done
-
-    if pull=$(git pull --all --rebase 2>&1); then
-      printf '%s
-    ' "$pull"
-    else
-      echo "git pull failed; exited with $?" >&2
-      exit 1
-    fi
-
-    if lfs_pull=$(git lfs pull 2>&1); then
-      printf '%s
-    ' "$lfs_pull"
-    else
-      echo "git lfs pull failed; exited with $?" >&2
-      exit 1
-    fi
-
+    cd branches
+    refs_array=($(cat refs.txt))
+    hashes_array=($(cat hashes.txt))
     cd ..
+    cd $REPO_MIRROR
 
-    if fetch=$(node fetch.js 2>&1); then
-      printf '%s
-    ' "$fetch"
-    else
-      echo "node fetch.js failed; exited with $?" >&2
-      exit 1
-    fi
-
-    if rewriteRefs=$(node rewriteRefs.js 2>&1); then
-      printf '%s
-    ' "$rewriteRefs"
-    else
-      echo "node rewriteRefs.js failed; exited with $?" >&2
-      exit 1
-    fi
-
-    cd "$REPO_DIR"
-
-
-    branches=($(git for-each-ref --format='%(refname:lstrip=2)' refs/heads/ | sort))
-
-    sorted_branches=()
-    for branch in "${branches[@]}"
-    do
-      commit_count=$(git rev-list --count "refs/heads/$branch")
-      sorted_branches+=("$commit_count:$branch")
-    done
-
-    sorted_branches_sorted=($(echo "${sorted_branches[@]}" | tr ' ' '\n' | sort -nr | cut -d':' -f2))
 
     # Parallelization is not fully tested yet
-    # export -f push_small_commits_in_parallel
+    export -f push_small_commits_in_parallel
 
-    num_branches=0
-    for ((; num_branches<${#sorted_branches_sorted[@]}; num_branches+=1)); do
-        push_small_commits_to_branch "${sorted_branches_sorted[num_branches]}"
+    batch_num=0
+    batch_size=8
+    for ((; batch_num<$((${#refs_array[@]} / ${batch_size} )); batch_num+=1)); do
         # Parallelization is not fully tested yet
-        # if (($num_branches % 3 == 0 || $num_branches == 0)); then
-        #     arr=()
-        #     arr+=("${sorted_branches_sorted[num_branches]}")
-        #     arr+=("${sorted_branches_sorted[num_branches+1]}")
-        #     arr+=("${sorted_branches_sorted[num_branches+2]}")
-        #     push_small_commits_in_parallel "${arr[@]}"
-        # fi
+        local refs_parrallel_arr=()
+        local hashes_parrallel_arr=()
+        for ((i=0; i<8; i++)); do
+          refs_parrallel_arr+=("${refs_array[batch_num+i]}")
+          hashes_parrallel_arr+=("${hashes_array[batch_num+i]}")
+        done
+        push_small_commits_in_parallel "${refs_parrallel_arr[@]}" "${hashes_parrallel_arr[@]}"
     done
 
-    # Parallelization is not fully tested yet
-    # if ((num_branches % 3 == 1)); then
-    #   push_small_commits_to_branch "${filtered_branches[num_branches-1]}"
-    # elif ((num_branches % 3 == 2)); then
-    #   push_small_commits_to_branch "${filtered_branches[num_branches-2]}"
-    #   push_small_commits_to_branch "${filtered_branches[num_branches-1]}"
+    # Push the remaining branches
+    local refs_parrallel_remaining_arr=()
+    local hashes_parrallel_remaining_arr=()
+    for ((i=0; i<((${#refs_array[@]} % ${batch_size} )); i++)); do
+      refs_parrallel_remaining_arr+=("${refs_array[batch_num+i]}")
+      hashes_parrallel_remaining_arr+=("${hashes_array[batch_num+i]}")
+    done
+    push_small_commits_in_parallel "${refs_parrallel_remaining_arr[@]}" "${hashes_parrallel_remaining_arr[@]}"
+
+    echo $(git push "$TARGET_SSH" --mirror)
+
+
+    # # Create a new directory to store the cloned repository and move the mirrored repository inside
+    # mkdir "$REPO_DIR"
+    # mv "$REPO_MIRROR" "$REPO_DIR"
+    # cd "$REPO_DIR"
+
+    # mv "$REPO_MIRROR" ".git"
+    # git init
+    # git config --bool core.bare false
+
+    # if fetch_all=$(git fetch --all --tags --update-head-ok 2>&1); then
+    #   printf '%s
+    #   ' "$fetch_all"
+    # else
+    #   echo "git fetch failed; exited with $?" >&2
+    #   exit 1
     # fi
 
 
+    # if fetch_lfs_all=$(git lfs fetch --all 2>&1); then
+    #   printf '%s
+    # ' "$fetch_lfs_all"
+    # else
+    #   echo "git lfs fetch failed; exited with $?" >&2
+    #   exit 1
+    # fi
+
+    # if pull=$(git pull --all --rebase 2>&1); then
+    #   printf '%s
+    # ' "$pull"
+    # else
+    #   echo "git pull failed; exited with $?" >&2
+    #   exit 1
+    # fi
+
+    # if lfs_pull=$(git lfs pull 2>&1); then
+    #   printf '%s
+    # ' "$lfs_pull"
+    # else
+    #   echo "git lfs pull failed; exited with $?" >&2
+    #   exit 1
+    # fi
+
     # # Push all changes to the remote repository
-    echo $(git push "$TARGET_SSH" --all)
-    echo $(git lfs push "$TARGET_SSH" --all)
-    cd ..
+    # echo $(git push "$TARGET_SSH" --all)
+    # echo $(git lfs push "$TARGET_SSH" --all)
+    # cd ..
 
-    if createBranches=$(node createBranches.js 2>&1); then
-      printf '%s
-    ' "$createBranches"
-    else
-      echo "node createBranches.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if createBranches=$(node createBranches.js 2>&1); then
+    #   printf '%s
+    # ' "$createBranches"
+    # else
+    #   echo "node createBranches.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if createIssues=$(node createIssues.js 2>&1); then
-      printf '%s
-    ' "$createIssues"
-    else
-      echo "node createIssues.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if createIssues=$(node createIssues.js 2>&1); then
+    #   printf '%s
+    # ' "$createIssues"
+    # else
+    #   echo "node createIssues.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if createComment=$(node createComments.js 2>&1); then
-      printf '%s
-    ' "$createComment"
-    else
-      echo "node createComments.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if createComment=$(node createComments.js 2>&1); then
+    #   printf '%s
+    # ' "$createComment"
+    # else
+    #   echo "node createComments.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if updateIssue=$(node updateIssues.js 2>&1); then
-      printf '%s
-    ' "$updateIssue"
-    else
-      echo "node updateIssues.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if updateIssue=$(node updateIssues.js 2>&1); then
+    #   printf '%s
+    # ' "$updateIssue"
+    # else
+    #   echo "node updateIssues.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if deleteBranches=$(node deleteBranches.js 2>&1); then
-      printf '%s
-    ' "$deleteBranches"
-    else
-      echo "node deleteBranches.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if deleteBranches=$(node deleteBranches.js 2>&1); then
+    #   printf '%s
+    # ' "$deleteBranches"
+    # else
+    #   echo "node deleteBranches.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if createReleases=$(node createReleases.js 2>&1); then
-      printf '%s
-    ' "$createReleases"
-    else
-      echo "node createReleases.js failed; exited with $?" >&2
-      exit 1
-    fi
-
-    echo "Migration completed successfully!"
-    display_txt_file "mettons.txt"
-
-    # createIssues=$(node createIssues.js)
-    # echo "$createIssues"
-
-    # createComment=$(node createComments.js)
-    # echo "$createComment"
-
-    # updateIssue=$(node updateIssues.js)
-    # echo "$updateIssue"
-
-    # deleteBranches=$(node deleteBranches.js)
-    # echo "$deleteBranches"
-
-    # createReleases=$(node createReleases.js)
-    # echo "$createReleases"
+    # if createReleases=$(node createReleases.js 2>&1); then
+    #   printf '%s
+    # ' "$createReleases"
+    # else
+    #   echo "node createReleases.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
     # echo "Migration completed successfully!"
     # display_txt_file "mettons.txt"
-
 }
 
 main
