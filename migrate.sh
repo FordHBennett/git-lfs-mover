665e9ccb6623b4c86262b4b24627182aeefd8072
Ford Hideo Bennett <ford660bennett@tamu.edu>
2023-04-26 17:09:27 -0500

    push_small_commits_to_branch() optimization

--- migrate.sh
+++ migrate.sh
@@ -33,8 +33,8 @@ push_small_commits_to_branch()
     local branch="$1"
     local hash="$2"
 
-    echo "$(git checkout "$branch")"
-    echo "$(git lfs fetch --all "$branch")"
+    echo "$(git checkout -f "$branch")"
+    echo "$(git lfs fetch "$branch")"
     echo "$(git lfs checkout)"
 
     echo $(git push "$TARGET_SSH" "$(git rev-list --reverse --max-count=1 $hash):$branch" )
@@ -44,9 +44,13 @@ push_small_commits_to_branch()
     # the total size is greater than 2GiB, then push it to the given branch up
     # to the commit before the total size was greater than 2Gib.
     local hash_size=0
-    local commit_array=$(git rev-list --reverse $hash)
+    local commit_array=()
+    while read -r line; do
+      commit_array+=("$line")
+    done < <(git rev-list --reverse $hash)
     for ((i=0;i<${#commit_array[@]}; i+=1)); do
-        hash_size+=$(git cat-file -s ${commit_array[i]})
+        object_size=$(git cat-file -s ${commit_array[i]})
+        hash_size=$((hash_size + object_size))
         if [ $hash_size -gt 2147483648 ]; then
           echo $(git push --force "$TARGET_SSH" "${commit_array[i-1]}:$branch")
         fi
@@ -85,92 +89,79 @@ display_txt_file() {
 }
 main()
 {
-    # Run the node test.js script
-    echo "$(src_test)"
-    echo "$(target_test)"
+    # # Run the node test.js script
+    # echo "$(src_test)"
+    # echo "$(target_test)"
 
 
-    if clone_mirror=$(git clone --mirror "$SRC_SSH" "$REPO_MIRROR" >/dev/null 2>&1); then
-      printf '%s
-    ' "$clone_mirror"
-      echo "Successfully cloned $SRC_SSH to $REPO_MIRROR"
-    else
-      echo "Failed to clone $SRC_SSH; exited with $?" >&2
-      exit 1
-    fi
+    # if clone_mirror=$(git clone --mirror "$SRC_SSH" "$REPO_MIRROR" >/dev/null 2>&1); then
+    #   printf '%s
+    # ' "$clone_mirror"
+    #   echo "Successfully cloned $SRC_SSH to $REPO_MIRROR"
+    # else
+    #   echo "Failed to clone $SRC_SSH; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if fetch=$(node fetch.js 2>&1); then
-      printf '%s
-    ' "$fetch"
-    else
-      echo "node fetch.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if fetch=$(node fetch.js 2>&1); then
+    #   printf '%s
+    # ' "$fetch"
+    # else
+    #   echo "node fetch.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if rewriteRefs=$(node rewriteRefs.js 2>&1); then
-      printf '%s
-    ' "$rewriteRefs"
-    else
-      echo "node rewriteRefs.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if rewriteRefs=$(node rewriteRefs.js 2>&1); then
+    #   printf '%s
+    # ' "$rewriteRefs"
+    # else
+    #   echo "node rewriteRefs.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
 
-    # Create a new directory to store the cloned repository and move the mirrored repository inside
-    # mkdir "$REPO_DIR"
-    mv "$REPO_MIRROR" "$REPO_DIR"
+    # # Create a new directory to store the cloned repository and move the mirrored repository inside
+    # # mkdir "$REPO_DIR"
+    # mv "$REPO_MIRROR" "$REPO_DIR"
+    # cd "$REPO_DIR"
+
+    # mv "$REPO_MIRROR" ".git"
+    # echo $(git init)
+    # git config --bool core.bare false
+
     cd "$REPO_DIR"
-
-    mv "$REPO_MIRROR" ".git"
-    echo $(git init)
-    git config --bool core.bare false
-
+    cd ".git"
     refs=$(cat packed-refs | awk '$2 ~ /^refs\/heads/{print $2}')
     refs_array=($refs)
     hashes=$(cat packed-refs | awk '$2 ~ /^refs\/heads/{print $1}')
     hashes_array=($hashes)
-    for((i=0; i<${#refs_array[@]}; i++)); do
-      if [[ ! "${refs_array[i]}" =~ "HEAD" ]]; then
-        local_branch=${"${refs_array[i]}"#remotes/origin/} # remove the remote prefix
-        if [[ ! "$local_branch" =~ ^master$|^dev$ ]]; then # skip master and dev branch
-          echo "$(git checkout -f -b "$local_branch" "${hashes_array[i]}")"
-          echo $(git branch --track "$local_branch" "$branch" )
-        fi
-      fi
-    done
+    cd ..
+    # for((i=0; i<${#refs_array[@]}; i++)); do
+    #   if [[ ! "${refs_array[i]}" =~ "HEAD" ]]; then
+    #     local_branch=${"${refs_array[i]}"#remotes/origin/} # remove the remote prefix
+    #     if [[ ! "$local_branch" =~ ^master$|^dev$ ]]; then # skip master and dev branch
+    #       echo "$(git checkout -f -b "$local_branch" "${hashes_array[i]}")"
+    #       echo $(git branch --track "$local_branch" "$branch" )
+    #     fi
+    #   fi
+    # done
 
 
-    if fetch_all=$(git fetch --all --tags --update-head-ok 2>&1); then
-      printf '%s
-      ' "$fetch_all"
-    else
-      echo "git fetch failed; exited with $?" >&2
-      exit 1
-    fi
+    # if fetch_all=$(git fetch --all --tags --update-head-ok 2>&1); then
+    #   printf '%s
+    #   ' "$fetch_all"
+    # else
+    #   echo "git fetch failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if fetch_lfs_all=$(git lfs fetch --all 2>&1); then
-      printf '%s
-    ' "$fetch_lfs_all"
-    else
-      echo "git lfs fetch failed; exited with $?" >&2
-      exit 1
-    fi
-
-    if pull=$(git pull --all --rebase 2>&1); then
-      printf '%s
-    ' "$pull"
-    else
-      echo "git pull failed; exited with $?" >&2
-      exit 1
-    fi
-
-    if lfs_pull=$(git lfs pull 2>&1); then
-      printf '%s
-    ' "$lfs_pull"
-    else
-      echo "git lfs pull failed; exited with $?" >&2
-      exit 1
-    fi
+    # if fetch_lfs_all=$(git lfs fetch --all 2>&1); then
+    #   printf '%s
+    # ' "$fetch_lfs_all"
+    # else
+    #   echo "git lfs fetch failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
     # cd ..
     # mkdir "branches"
@@ -214,60 +205,60 @@ main()
     # push_small_commits_in_parallel "${refs_parrallel_remaining_arr[@]}" "${hashes_parrallel_remaining_arr[@]}"
 
     # Push all changes to the remote repository
-    echo $(git push "$TARGET_SSH" --all)
-    echo $(git lfs push "$TARGET_SSH" --all)
-    cd ..
+    # echo $(git push "$TARGET_SSH" --all)
+    # echo $(git lfs push "$TARGET_SSH" --all)
+    # cd ..
 
-    if createBranches=$(node createBranches.js 2>&1); then
-      printf '%s
-    ' "$createBranches"
-    else
-      echo "node createBranches.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if createBranches=$(node createBranches.js 2>&1); then
+    #   printf '%s
+    # ' "$createBranches"
+    # else
+    #   echo "node createBranches.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if createIssues=$(node createIssues.js 2>&1); then
-      printf '%s
-    ' "$createIssues"
-    else
-      echo "node createIssues.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if createIssues=$(node createIssues.js 2>&1); then
+    #   printf '%s
+    # ' "$createIssues"
+    # else
+    #   echo "node createIssues.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if createComment=$(node createComments.js 2>&1); then
-      printf '%s
-    ' "$createComment"
-    else
-      echo "node createComments.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if createComment=$(node createComments.js 2>&1); then
+    #   printf '%s
+    # ' "$createComment"
+    # else
+    #   echo "node createComments.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if updateIssue=$(node updateIssues.js 2>&1); then
-      printf '%s
-    ' "$updateIssue"
-    else
-      echo "node updateIssues.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if updateIssue=$(node updateIssues.js 2>&1); then
+    #   printf '%s
+    # ' "$updateIssue"
+    # else
+    #   echo "node updateIssues.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if deleteBranches=$(node deleteBranches.js 2>&1); then
-      printf '%s
-    ' "$deleteBranches"
-    else
-      echo "node deleteBranches.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if deleteBranches=$(node deleteBranches.js 2>&1); then
+    #   printf '%s
+    # ' "$deleteBranches"
+    # else
+    #   echo "node deleteBranches.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    if createReleases=$(node createReleases.js 2>&1); then
-      printf '%s
-    ' "$createReleases"
-    else
-      echo "node createReleases.js failed; exited with $?" >&2
-      exit 1
-    fi
+    # if createReleases=$(node createReleases.js 2>&1); then
+    #   printf '%s
+    # ' "$createReleases"
+    # else
+    #   echo "node createReleases.js failed; exited with $?" >&2
+    #   exit 1
+    # fi
 
-    echo "Migration completed successfully!"
-    display_txt_file "mettons.txt"
+    # echo "Migration completed successfully!"
+    # display_txt_file "mettons.txt"
 }
 
 main
